3a69c153c0a689d49039def5fd6a921cb88c6390
刘冯吉 <253916036@qq.com>
2025-08-11 15:22:43 +0800

    chore:update mainfest

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class BotDescParameter(BasePlugin):
             query.set_variable("adapter_config", config)
 
             # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
+            ctx.add_return("reply", ["hello, {}!,{}".format(ctx.event.sender_id,config)])
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
